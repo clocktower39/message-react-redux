dbf33d09404c3845d4de8e60b2c4e345cbcbe3f4
clocktower39 <clocktower39@aol.com>
2021-05-14 23:46:42 -0700

    error clean up

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState }  from 'react';
 import { connect, useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { Button, TextField, Grid, Paper, makeStyles } from '@material-ui/core';
