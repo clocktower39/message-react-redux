a1ca13c2a56e9028ecbb1face3ea81b39e4f6a70
clocktower39 <clocktower39@aol.com>
2021-06-10 00:10:39 -0700

    TextField account page

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Grid, Typography, makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Grid, TextField, Typography, makeStyles } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 const useStyles = makeStyles({
@@ -10,18 +10,21 @@ const useStyles = makeStyles({
 
 export default function Account(props) {
     const user = useSelector(state => state.user);
+
+    const [username, setUsername] = useState(user.username);
+    const [firstName, setFirstName] = useState(user.firstName);
+    const [lastName, setLastName] = useState(user.lastName);
+    const [email, setEmail] = useState(user.email);
+
     const classes = useStyles();
+
     return (
         <div className={classes.root}>
-            <Grid container>
-            <Grid item xs={6}><Typography variant="body1">user.username</Typography></Grid>
-            <Grid item xs={6}><Typography variant="body1">{user.username}</Typography></Grid>
-            <Grid item xs={6}><Typography variant="body1">user.firstName</Typography></Grid>
-            <Grid item xs={6}><Typography variant="body1">{user.firstName}</Typography></Grid>
-            <Grid item xs={6}><Typography variant="body1">user.lastName</Typography></Grid>
-            <Grid item xs={6}><Typography variant="body1">{user.lastName}</Typography></Grid>
-            <Grid item xs={6}><Typography variant="body1">user.email</Typography></Grid>
-            <Grid item xs={6}><Typography variant="body1">{user.email}</Typography></Grid>
+            <Grid container spacing={3} justify="center">
+                <Grid item xs={12}><TextField label="Username" variant="outlined" value={username} fullWidth/></Grid>
+                <Grid item xs={12}><TextField label="First Name" variant="outlined" value={firstName} fullWidth/></Grid>
+                <Grid item xs={12}><TextField label="Last Name" variant="outlined" value={lastName} fullWidth/></Grid>
+                <Grid item xs={12}><TextField label="Email" variant="outlined" value={email} fullWidth/></Grid>
             </Grid>
         </div>
     )
