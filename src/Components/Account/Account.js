7b5d0ea9bbd961058d624f0b5a768daf6bc7ff9c
clocktower39 <clocktower39@aol.com>
2021-06-10 22:49:22 -0700

    onChange account page

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -1,31 +1,65 @@
-import React, { useState } from 'react';
-import { Grid, TextField, Typography, makeStyles } from '@material-ui/core';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { Grid, TextField, makeStyles } from "@material-ui/core";
+import { useSelector } from "react-redux";
 
 const useStyles = makeStyles({
-    root:{
-        paddingTop: '100px',
-    }
-})
+  root: {
+    paddingTop: "100px",
+  },
+});
 
 export default function Account(props) {
-    const user = useSelector(state => state.user);
+  const user = useSelector((state) => state.user);
 
-    const [username, setUsername] = useState(user.username);
-    const [firstName, setFirstName] = useState(user.firstName);
-    const [lastName, setLastName] = useState(user.lastName);
-    const [email, setEmail] = useState(user.email);
+  const [username, setUsername] = useState(user.username);
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [email, setEmail] = useState(user.email);
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <Grid container spacing={3} justify="center">
-                <Grid item xs={12}><TextField label="Username" variant="outlined" value={username} fullWidth/></Grid>
-                <Grid item xs={12}><TextField label="First Name" variant="outlined" value={firstName} fullWidth/></Grid>
-                <Grid item xs={12}><TextField label="Last Name" variant="outlined" value={lastName} fullWidth/></Grid>
-                <Grid item xs={12}><TextField label="Email" variant="outlined" value={email} fullWidth/></Grid>
-            </Grid>
-        </div>
-    )
+  const handleChange = (e, setter) => setter(e.target.value);
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12}>
+          <TextField
+            label="Username"
+            variant="outlined"
+            value={username}
+            fullWidth
+            onChange={(e) => handleChange(e, setUsername)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label="First Name"
+            variant="outlined"
+            value={firstName}
+            fullWidth
+            onChange={(e) => handleChange(e, setFirstName)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label="Last Name"
+            variant="outlined"
+            value={lastName}
+            fullWidth
+            onChange={(e) => handleChange(e, setLastName)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label="Email"
+            variant="outlined"
+            value={email}
+            fullWidth
+            onChange={(e) => handleChange(e, setEmail)}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
