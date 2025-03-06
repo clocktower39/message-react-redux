811794616c4f646527c9ae0e144559ca9c0b727f
clocktower39 <clocktower39@aol.com>
2025-03-05 22:56:04 -0700

    configure chunking

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/message/",
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          "mui-vendor": [
+            "@mui/material",
+            "@mui/icons-material",
+            "@mui/styles",
+            "@emotion/react",
+            "@emotion/styled",
+          ],
+          "redux-vendor": ["react-redux", "redux", "redux-thunk", "@redux-devtools/extension"],
+          socket: ["socket.io-client"],
+          utils: ["axios", "jwt-decode"],
+        },
+      },
+    },
+  },
+});
